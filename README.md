691e16e22fe67202632795c4e2dd186dab15eabd
mochadreamboat <jersotmac@gmail.com>
2022-04-07 00:47:27 -0500

    Added considerations for uniquely ID'ing task objects and creating event listeners targeting specific DOMS for deletion.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Idea-Den
+Brain dump for project ideas I'm working on in Ubuntu while I'm booting Windows.
---
+++ To-Do-List/script.js
@@ -0,0 +1,27 @@
+// Assign a random ID to newly generated task as a property value.
+// This value will act as an attribute for the actual HTML element.
+// [data-id = *random-number*]
+
+// Event listener will target DOM element based on assigned querySelector (pertaining to data-id)
+// Remove child with respective data-id.
+
+// To be used in object constructor property (id);
+function guidGenerator() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+// Include somewhere in generate task
+task = document.querySelector('task');
+task.setAttribute("id", `${objectid}`);
+
+// Event Listener for removing task from DOM.
+// Unique to each task in DOM. Task generator will pass object's id as argument to
+// deleteTask() and will trigger on click.
+
+function deleteTask (id) {
+let removedTask = document.getElementById(id);
+tasks.removeChild(removeTask);
+}
